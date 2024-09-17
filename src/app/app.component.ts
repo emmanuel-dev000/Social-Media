06c50f7e37b5dbfac206140eac1a43159dd88dd5
Armanlone <armanlonedev@gmail.com>
2024-09-17 15:54:25 +0800

    fetch get and post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'social-media';
+  data = {
+    userId: 1,
+    id: 1,
+    title: "Love Live Superstar",
+    completed: false
+  };
+
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.data)
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error(err));
+  }
 }
